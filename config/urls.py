220e8a9c709780c7076aa094d3b35daacf3cef01
dev-rastogi03 <devrastogi433@gmail.com>
2026-09-23 22:11:09 +0530

    Fix: Route /media/ uploaded images in production environment

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,12 @@ URL configuration for Rastogi Traders agricultural website.
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.sitemaps.views import sitemap
+from django.views.static import serve
 from django.views.generic.base import TemplateView
+from django.contrib.sitemaps.views import sitemap
 from .sitemaps import (
     StaticViewSitemap, ProductSitemap, CategorySitemap, 
     CropGuideSitemap, FarmerTipSitemap
@@ -28,12 +29,13 @@ urlpatterns = [
     path('farmer-corner/', include('farmers.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'), name='robots_txt'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Serve media and static files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'core.views.custom_404_view'
 handler500 = 'core.views.custom_500_view'
+
+
